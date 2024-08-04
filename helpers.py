e3e3d44e6cdfbf648266d5a6c4ee927b480f0e19
sandry24 <sandry.grosu.24@gmail.com>
2024-08-04 20:57:33 +0300

    Remove apology special character escaping

--- helpers.py
+++ helpers.py
@@ -4,27 +4,7 @@ from functools import wraps
 
 def apology(message, code=400):
     """Render message as an apology to user."""
-
-    def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [
-            ("-", "--"),
-            (" ", "-"),
-            ("_", "__"),
-            ("?", "~q"),
-            ("%", "~p"),
-            ("#", "~h"),
-            ("/", "~s"),
-            ('"', "''"),
-        ]:
-            s = s.replace(old, new)
-        return s
-
-    return render_template("apology.html", code=code, message=escape(message)), code
+    return render_template("apology.html", code=code, message=message), code
 
 
 def login_required(f):
