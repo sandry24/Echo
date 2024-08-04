1ebe22c62a655bdaece3b62ff3b754794f1ff877
sandry24 <sandry.grosu.24@gmail.com>
2024-08-04 16:13:45 +0300

    Add functionality to login and register screen

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+db = SQL("sqlite:///echo.db")
+
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -28,10 +31,70 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """Log user in"""
     session.clear()
 
     if request.method == "POST":
-        return apology("TODO")
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?",
+            request.form.get("username")
+        )
+
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 400)
+
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
     else:
         return render_template("login.html")
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords do not match", 400)
+
+        # Ensure this username does not exist
+        try:
+            db.execute(
+                "INSERT INTO users (username, hash) VALUES (?, ?)",
+                request.form.get("username"),
+                generate_password_hash(request.form.get("password"))
+            )
+
+            rows = db.execute(
+                "SELECT * FROM users WHERE username = ?", request.form.get("username")
+            )
+
+            session["user_id"] = rows[0]["id"]
+            flash(f"Successfully registered!")
+        except ValueError:
+            return apology("username already exists", 400)
+
+        return redirect("/")
+    else:
+        return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+    session.clear()
+    return redirect("/")
